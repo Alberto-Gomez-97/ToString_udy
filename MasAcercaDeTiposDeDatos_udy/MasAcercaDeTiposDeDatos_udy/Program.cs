53a0368516f98ff905428f8cd96d49626944591b
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-11 16:52:48 -0600

    Add files via upload

---
+++ MasAcercaDeTiposDeDatos_udy/MasAcercaDeTiposDeDatos_udy/Program.cs
@@ -0,0 +1,18 @@
+﻿//ToString a string o hacia string
+
+var cant = 5; //int
+//para invocar una funcionalidad requieren parentesis
+if (cant.ToString() == "5") //convertido a string
+{
+    Console.WriteLine("Son iguales");
+}
+else
+{
+    Console.WriteLine("No son iguales");
+}
+
+var esCierto = true;
+esCierto.ToString();
+// ToString "" se puede escoger el tipo de fomato
+var hoy = DateTime.Today.ToString("yyyy-MM-dd (dddd)");
+Console.WriteLine(hoy);
